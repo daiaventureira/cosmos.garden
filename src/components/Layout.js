d6f0f142f4d4c53d9591b42a9c99f9cb817af44b
Daiane Souza <dai2704@gmail.com>
2020-05-02 23:16:50 -0400

    initial layout

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import index from './index.scss';
+
+const Layout = props => {
+    return(
+    <div className="container">
+        <div className="main-margins">
+            <div className="diamond-shape"></div>
+            <div className="outside-margin-right"></div>
+            <div className="outside-margin-left"></div>   
+        </div>
+    </div>
+    )
+}
+
+export default Layout;
